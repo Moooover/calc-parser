cb5af34076e75253f9fbcfaaa99cff879a6abca3
Moooover <kevin.cheng319@gmail.com>
2023-02-07 08:47:11 +0000

    example aug_bnf

--- aug_bnf/src/lib.rs
+++ aug_bnf/src/lib.rs
@@ -1,13 +1,36 @@
 /// Constructs an AugBnf parser based on the definition provided.
 pub use aug_bnf_impl::aug_bnf;
 
-pub fn test_fn() {
-  aug_bnf!(
-    fn answer() -> i32 {
-      43 | 32
+enum RequestType {
+  GET,
+  HEAD,
+}
+
+struct Req {
+  req_type: RequestType,
+  uri: String,
+}
+
+impl Req {
+  pub fn new(req_type: RequestType, uri: &str) -> Self {
+    Self {
+      req_type,
+      uri
     }
+  }
+}
+
+pub fn test_fn() {
+  let parser = aug_bnf!(
+    absoluteURI = re":[a-zA-Z/]+";
+    req = "GET" { Request::GET } | "HEAD" { Request::HEAD };
+    uri = absoluteURI {
+      absoluteURI
+    };
+    header = req uri {
+      Req::new(req, uri)
+    };
   );
-  let result = bnswer();
 }
 
 #[cfg(test)]

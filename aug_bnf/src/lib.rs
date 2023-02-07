c54044cf0a99910bd7c9d6e82c5e043318c55763
Moooover <kevin.cheng319@gmail.com>
2023-02-07 08:56:08 +0000

    more desc

--- aug_bnf/src/lib.rs
+++ aug_bnf/src/lib.rs
@@ -22,13 +22,19 @@ impl Req {
 
 pub fn test_fn() {
   let parser = aug_bnf!(
+    // if no {} given, return entire consumed text.
+    // a re before text means to treat the text as regex.
     absoluteURI = re":[a-zA-Z/]+";
+    // an evaluable block may be given for each option of a |
     req = "GET" { Request::GET } | "HEAD" { Request::HEAD };
+    // parameter names are by default $<position>.
     uri = absoluteURI {
-      absoluteURI
+      $1
     };
+    // the root rule is determined via the dependency graph: only one rule may
+    // not be referred to by any others, and it is the root.
     header = req uri {
-      Req::new(req, uri)
+      Req::new($1, $2)
     };
   );
 }
